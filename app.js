62839ba02c8c644aa559354adce6f5b5ddd61b80
Samantha <178785253+fenrissa92@users.noreply.github.com>
2024-12-17 14:59:44 +0000

    ticket 3 and 4 solved

--- app.js
+++ app.js
@@ -16,6 +16,22 @@ app.get("/", function (req, res) {
   res.send("Welcome to the inspirational quotes API");
 });
 
+
+//ticket3
+app.get("/quotes", async function (req, res) {
+    const quotes = await getQuotes();
+    res.json(quotes);
+});
+
+
+//ticket 4
+app.get("/quotes/:id", async function(req, res) {
+  const id = req.params.id;
+  const quote = await getQuoteByID(id);
+  res.json(quote);
+});
+
+
 app.listen(PORT, function () {
   console.log(`Server is now listening on http://localhost:${PORT}`);
 });
